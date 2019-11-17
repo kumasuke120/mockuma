09d2130ed633618c47123af99b7ff06da1a16b28
Kumasuke <bearcomingx@gmail.com>
2019-11-17 23:37:38 +0800

    fix(load): '-mapfile' now works properly with relative path

--- cmd/main.go
+++ cmd/main.go
@@ -3,29 +3,19 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/kumasuke120/mockuma/internal/mapping"
 	"github.com/kumasuke120/mockuma/internal/serve"
 )
 
-const defaultMapfile = "mockuMappings.json"
-
 var port = flag.Int("p", 3214,
 	"the port number on which Mockuma listen")
-var mapfile = flag.String("mapfile", "mockuMappings.json",
+var mapfile = flag.String("mapfile", mapping.DefaultMapfile,
 	"the name of a json file which defines mockuMappings")
 
-func init() {
+func main() {
 	flag.Parse()
 
-	if *mapfile != defaultMapfile {
-		setWorkingDirBasedOnMapfile()
-	}
-}
-
-func main() {
 	mappings, err := mapping.FromJsonFile(*mapfile)
 	if err != nil {
 		log.Fatal("cannot load mockuMappings:", err)
@@ -35,19 +25,4 @@ func main() {
 	if err := server.Start(); err != nil {
 		log.Fatal("cannot start server:", err)
 	}
-}
-
-func setWorkingDirBasedOnMapfile() {
-	abs, err := filepath.Abs(*mapfile)
-	if err != nil {
-		log.Fatal("Cannot acquire the absolute path of mockuMappings:", err)
-	}
-
-	dir := filepath.Dir(abs)
-	err = os.Chdir(dir)
-	if err != nil {
-		log.Fatal("Cannot change the working directory:", err)
-	}
-
-	log.Println("[main] working directory has been changed to:", dir)
 }
