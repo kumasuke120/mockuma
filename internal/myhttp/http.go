297bfaa4b6152df20e682a6d0a6b3691b5cdb0d9
Kumasuke <bearcomingx@gmail.com>
2019-11-17 22:32:11 +0800

    feat(http): supports all http/1.1 methods

--- internal/myhttp/http.go
+++ internal/myhttp/http.go
@@ -8,10 +8,14 @@ type HttpMethod int
 
 const (
 	Any = iota
+	Options
 	Get
+	Head
 	Post
 	Put
 	Delete
+	Trace
+	Connect
 )
 
 type StatusCode int
@@ -33,14 +37,22 @@ func ToHttpMethod(method interface{}) HttpMethod {
 	switch method.(type) {
 	case string:
 		switch strings.ToUpper(method.(string)) {
+		case "OPTIONS":
+			return Options
 		case "GET":
 			return Get
+		case "HEAD":
+			return Head
 		case "POST":
 			return Post
 		case "PUT":
 			return Put
 		case "DELETE":
 			return Delete
+		case "TRACE":
+			return Trace
+		case "CONNECT":
+			return Connect
 		default:
 			return Any
 		}
@@ -57,14 +69,22 @@ func (m HttpMethod) String() string {
 	switch m {
 	case Any:
 		return "Any"
+	case Options:
+		return "OPTIONS"
 	case Get:
 		return "GET"
+	case Head:
+		return "HEAD"
 	case Post:
 		return "POST"
 	case Put:
 		return "PUT"
 	case Delete:
 		return "DELETE"
+	case Trace:
+		return "TRACE"
+	case Connect:
+		return "Connect"
 	default:
 		panic("Should't happen")
 	}
