122780c1bd0c7bf27998c61dd1833abcdced87a3
kumasuke120 <bearcomingx@gmail.com>
2019-12-13 10:04:55 +0800

    test(template): adds test cases

--- internal/mckmaps/template_test.go
+++ internal/mckmaps/template_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 var theContext = &renderContext{filename: "test"}
-var theJsonPath = myjson.NewPath()
 var theVars = &vars{table: map[string]interface{}{
 	"a":  myjson.String("123"),
 	"b1": myjson.Number(123),
@@ -21,14 +20,80 @@ func TestRenderString(t *testing.T) {
 	assert := assert.New(t)
 
 	t1 := myjson.String("a = \"@{a}\", b1 = @{b1}, b2 = @{b2}, c = @{c}")
-	r1, e1 := renderString(theContext, theJsonPath, t1, theVars)
-	assert.Nil(e1)
-	assert.IsType(myjson.String(""), r1)
-	assert.EqualValues("a = \"123\", b1 = 123, b2 = 123.123, c = true", r1)
+	jp1 := myjson.NewPath()
+	r1, e1 := renderString(theContext, jp1, t1, theVars)
+	if assert.Nil(e1) {
+		assert.IsType(myjson.String(""), r1)
+		assert.EqualValues("a = \"123\", b1 = 123, b2 = 123.123, c = true", r1)
+	}
 
 	t2 := myjson.String("a = \"@{a:%8s}\", b1 = ORD@{b1:%010d}, b2 = @{b2:%.2f}, c = @{c}")
-	r2, e2 := renderString(theContext, theJsonPath, t2, theVars)
-	assert.Nil(e2)
-	assert.IsType(myjson.String(""), r2)
-	assert.EqualValues("a = \"     123\", b1 = ORD0000000123, b2 = 123.12, c = true", r2)
+	jp2 := myjson.NewPath()
+	r2, e2 := renderString(theContext, jp2, t2, theVars)
+	if assert.Nil(e2) {
+		assert.IsType(myjson.String(""), r2)
+		assert.EqualValues("a = \"     123\", b1 = ORD0000000123, b2 = 123.12, c = true", r2)
+	}
+
+	t3 := myjson.String("a = @@{a}")
+	jp3 := myjson.NewPath()
+	r3, e3 := renderString(theContext, jp3, t3, theVars)
+	if assert.Nil(e3) {
+		assert.IsType(myjson.String(""), r3)
+		assert.EqualValues("a = @{a}", r3)
+	}
+}
+
+func TestRenderObject(t *testing.T) {
+	//noinspection GoImportUsedAsName
+	assert := assert.New(t)
+
+	t1 := myjson.Object{
+		"a": myjson.String("@{a}"),
+		"b": myjson.Object{
+			"b1": myjson.String("@{b1}"),
+			"b2": myjson.String("@{b2}"),
+		},
+		"c": myjson.String("@{c}"),
+	}
+	jp1 := myjson.NewPath()
+	r1, e1 := renderObject(theContext, jp1, t1, theVars)
+	if assert.Nil(e1) {
+		expected1 := myjson.Object{
+			"a": myjson.String("123"),
+			"b": myjson.Object{
+				"b1": myjson.Number(123),
+				"b2": myjson.Number(123.123),
+			},
+			"c": myjson.Boolean(true),
+		}
+		assert.Equal(expected1, r1)
+	}
+}
+
+func TestRenderArray(t *testing.T) {
+	//noinspection GoImportUsedAsName
+	assert := assert.New(t)
+
+	t1 := myjson.Array{
+		myjson.String("@{a}"),
+		myjson.Array{
+			myjson.String("@{b1}"),
+			myjson.String("@{b2}"),
+		},
+		myjson.String("@{c}"),
+	}
+	jp1 := myjson.NewPath()
+	r1, e1 := renderArray(theContext, jp1, t1, theVars)
+	if assert.Nil(e1) {
+		expected1 := myjson.Array{
+			myjson.String("123"),
+			myjson.Array{
+				myjson.Number(123),
+				myjson.Number(123.123),
+			},
+			myjson.Boolean(true),
+		}
+		assert.Equal(expected1, r1)
+	}
 }
