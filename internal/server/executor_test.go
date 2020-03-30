130c1710dc8b44cfff29cf35c0845cf4f6c44881
kumasuke120 <bearcomingx@gmail.com>
2020-03-30 16:09:45 +0800

    test(executor): renames file and adds tests

--- internal/server/executor_test.go
+++ internal/server/executor_test.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -11,6 +13,12 @@ import (
 	"github.com/kumasuke120/mockuma/internal/myhttp"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestForwardError_Error(t *testing.T) {
+	e := &forwardError{err: errors.New("test_error")}
+	assert.NotNil(t, e)
+	assert.Contains(t, e.Error(), "test_error")
+}
 
 func TestWaitBeforeReturns(t *testing.T) {
 	//noinspection GoImportUsedAsName
@@ -44,8 +52,8 @@ func TestPolicyExecutor_executor(t *testing.T) {
 	}
 	e1 := exe1.execute()
 	if assert.Nil(e1) {
-		assert.Equal(rr1.Code, http.StatusOK)
-		assert.Equal(rr1.Body.String(), fmt.Sprintf(`{"statusCode": %d, "message": "%s"}`, http.StatusOK, "OK"))
+		assert.Equal(http.StatusOK, rr1.Code)
+		assert.Equal(fmt.Sprintf(`{"statusCode": %d, "message": "%s"}`, http.StatusOK, "OK"), rr1.Body.String())
 	}
 
 	rr2 := httptest.NewRecorder()
@@ -57,21 +65,23 @@ func TestPolicyExecutor_executor(t *testing.T) {
 	}
 	e2 := exe2.execute()
 	if assert.Nil(e2) {
-		assert.Equal(rr2.Code, http.StatusBadRequest)
-		assert.Equal(rr2.Body.String(), string(pNoPolicyMatched.Returns.Body))
+		assert.Equal(http.StatusBadRequest, rr2.Code)
+		assert.Equal(string(pNoPolicyMatched.Returns.Body), rr2.Body.String())
 	}
 
+	rr3Start := time.Now()
 	rr3 := httptest.NewRecorder()
 	var rw3 http.ResponseWriter = rr3
 	exe3 := &policyExecutor{
 		r: httptest.NewRequest("GET", "/TestPolicyExecutor_executor", nil),
 		w: &rw3,
 		policy: &mckmaps.Policy{
+			CmdType: mckmaps.CmdTypeReturns,
 			Returns: &mckmaps.Returns{
 				StatusCode: myhttp.StatusOk,
 				Latency: &mckmaps.Interval{
-					Min: 0,
-					Max: 0,
+					Min: 100,
+					Max: 100,
 				},
 				Headers: []*mckmaps.NameValuesPair{
 					{Name: myhttp.HeaderContentType, Values: []string{myhttp.ContentTypeJSON}},
@@ -82,4 +92,45 @@ func TestPolicyExecutor_executor(t *testing.T) {
 	}
 	e3 := exe3.execute()
 	assert.Nil(e3)
+	rr3Elapsed := time.Since(rr3Start)
+	assert.True(rr3Elapsed.Milliseconds() >= 100)
+
+	rr4 := httptest.NewRecorder()
+	var rw4 http.ResponseWriter = rr4
+	exe4 := &policyExecutor{
+		r: httptest.NewRequest("GET", "/TestPolicyExecutor_executor", nil),
+		w: &rw4,
+		policy: &mckmaps.Policy{
+			CmdType: mckmaps.CmdTypeForwards,
+			Forwards: &mckmaps.Forwards{
+				Latency: &mckmaps.Interval{
+					Min: 0,
+					Max: 0,
+				},
+				Path: "https://www.example.com",
+			},
+		},
+	}
+	e4 := exe4.execute()
+	assert.Nil(e4)
+	assert.Equal(http.StatusOK, rr4.Code)
+
+	handler := newMockHandler(mappings)
+	rr5 := httptest.NewRecorder()
+	var rw5 http.ResponseWriter = rr5
+	exe5 := &policyExecutor{
+		h: handler,
+		r: httptest.NewRequest("POST", "/TestPolicyExecutor_executor", bytes.NewReader([]byte("120"))),
+		w: &rw5,
+		policy: &mckmaps.Policy{
+			CmdType: mckmaps.CmdTypeForwards,
+			Forwards: &mckmaps.Forwards{
+				Path: "/m",
+			},
+		},
+	}
+	e5 := exe5.execute()
+	assert.Nil(e5)
+	assert.Equal(http.StatusOK, rr5.Code)
+	assert.Equal("TEST/v1", rr5.Header().Get("Server"))
 }
